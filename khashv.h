2a08fed88bba3014221bea5246b42e6a7724abf9
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2022-07-12 02:53:17 -0700

    Use GCC's vector builtins if available

--- khashv.h
+++ khashv.h
@@ -730,6 +730,163 @@ static uint64_t khashv64_vector(khashvSeed* seed, const uint8_t* data, size_t da
 
 #endif
 
+/* Vectorization via GCCs Vectorization builtins */
+
+#if !defined(KHASH_VECTOR) && KHASH_GCC_LEAST__(6, 1)
+
+#define KHASH_VECTOR 1
+
+typedef uint8_t  kv16ui __attribute__((vector_size(16)));
+typedef uint32_t kv4ui  __attribute__((vector_size(16)));
+
+static KHASH_FINLINE kv16ui khashv_sub_s1_gcc(kv16ui in) {
+    const kv16ui mask = {
+        0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf,
+        0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf, 0xf
+    };
+    const kv16ui sub = {
+        0x1c, 0x5d, 0xf8, 0xe3, 0xc1, 0x9c, 0xda, 0xb7,
+        0x63, 0x91, 0x59, 0xb3, 0x2b, 0xa5, 0xee, 0x12,
+    };
+    in &= mask;
+    return __builtin_shuffle(sub, in);
+}
+
+static KHASH_FINLINE kv16ui khashv_sub_s2_gcc(kv16ui in) {
+    const kv16ui sub = {
+        0xef, 0xce, 0x66, 0xf3, 0xf6, 0x21, 0x42, 0xa5,
+        0x11, 0xad, 0x5b, 0xc6, 0x72, 0x38, 0x95, 0x7a,
+    };
+    in >>= 4;
+    return __builtin_shuffle(sub, in);
+}
+
+static KHASH_FINLINE kv4ui khashv_rotr_5_bytes_gcc(kv4ui input) {
+    const kv16ui rotr = {
+        0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc,
+        0xd, 0xe, 0xf, 0x0, 0x1, 0x2, 0x3, 0x4
+    };
+    kv16ui tmp;
+    memcpy(&tmp, &input, 16);
+    tmp = __builtin_shuffle(tmp, rotr);
+    memcpy(&input, &tmp, 16);
+    return input;
+}
+
+static KHASH_FINLINE kv4ui khashv_rotr_9_bytes_gcc(kv4ui input) {
+    const kv16ui rotr = {
+        0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x0,
+        0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8,
+    };
+    kv16ui tmp;
+    memcpy(&tmp, &input, 16);
+    tmp = __builtin_shuffle(tmp, rotr);
+    memcpy(&input, &tmp, 16);
+    return input;
+}
+
+static KHASH_FINLINE kv4ui khashv_replace_gcc(kv4ui input) {
+    kv16ui s1;
+    kv16ui s2;
+    memcpy(&s1, &input, 16);
+    s2 = khashv_sub_s2_gcc(s1);
+    s1 = khashv_sub_s1_gcc(s1);
+    s1 ^= s2;
+    memcpy(&input, &s1, 16);
+    return input;
+}
+
+static KHASH_FINLINE kv4ui khashv_mix_words_gcc(kv4ui val) {
+    const unsigned rots[4] = { 5, 7, 11, 17 };
+    for (int i = 0; i < 4; i++) {
+        unsigned rot = rots[i];
+        kv4ui tmp = val;
+        tmp  = khashv_rotr_5_bytes_gcc(tmp);
+        tmp += val;
+        tmp  = (tmp >> rot) | (tmp << (32 - rot));
+        val ^= tmp;
+    }
+    return val;
+}
+
+static KHASH_FINLINE kv4ui khashv_hash_block_gcc(kv4ui hash, kv4ui input) {
+    kv4ui tmp_1 = khashv_replace_gcc(input);
+    kv4ui tmp_2 = tmp_1 * 8193;
+    tmp_2 ^= hash;
+    tmp_2  = khashv_rotr_5_bytes_gcc(tmp_2);
+    hash   = tmp_1 + tmp_2;
+
+    tmp_2  = hash >> 3;
+    tmp_1  = khashv_rotr_9_bytes_gcc(hash);
+    hash   = tmp_1 + tmp_2;
+    return hash;
+}
+
+static KHASH_FINLINE kv4ui khashv_hash_gcc(kv4ui hash, const uint8_t* data, size_t data_len) {
+    hash[0] ^= data_len;
+    #if defined(SIZE_MAX) && SIZE_MAX > 4294967295
+        hash[1] ^= data_len >> 32;
+    #endif
+
+    kv4ui data_v;
+    const uint8_t* end = data + (data_len & ~((size_t)15));
+    while (data < end) {
+        memcpy(&data_v, data, 16);
+        hash = khashv_hash_block_gcc(hash, data_v);
+        data += 16;
+    }
+
+    unsigned trailing = data_len & 0xf;
+    if(trailing) {
+        memset(&data_v, 0, 16);
+        memcpy(&data_v, data, trailing);
+        hash = khashv_hash_block_gcc(hash, data_v);
+    }
+    return khashv_mix_words_gcc(hash);
+}
+
+
+static void khashv_prep_seed32_vector(khashvSeed* seed_prepped, uint32_t seed) {
+    kv4ui s;
+    memcpy(&s, &khash_v_init, 16);
+    s[0] ^= seed;
+    s = khashv_mix_words_gcc(s);
+    memcpy(seed_prepped, &s, 16);
+}
+
+static void khashv_prep_seed64_vector(khashvSeed* seed_prepped, uint64_t seed) {
+    kv4ui s;
+    memcpy(&s, &khash_v_init, 16);
+    s[0] ^= seed;
+    s = khashv_mix_words_gcc(s);
+    s[1] ^= seed >> 32;
+    s = khashv_mix_words_gcc(s);
+    memcpy(seed_prepped, &s, 16);
+}
+
+static void khashv_prep_seed128_vector(khashvSeed* seed_prepped, const uint32_t seed[4]) {
+    memcpy(seed_prepped->words, seed, 16);
+}
+
+static uint32_t khashv32_vector(khashvSeed* seed, const uint8_t* data, size_t data_len) {
+    kv4ui h;
+    memcpy(&h, seed, 16);
+    h = khashv_hash_gcc(h, data, data_len);
+    return h[3];
+}
+
+static uint64_t khashv64_vector(khashvSeed* seed, const uint8_t* data, size_t data_len) {
+    kv4ui h;
+    memcpy(&h, seed, 16);
+    h = khashv_hash_gcc(h, data, data_len);
+    uint64_t ret = h[1];
+    ret <<= 32;
+    ret  |= h[0];
+    return ret;
+}
+
+#endif
+
 #if defined(KHASH_VECTOR)
 
     static void khashv_prep_seed32(khashvSeed* seed_prepped, uint32_t seed) {
