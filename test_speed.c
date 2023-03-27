8c4dba8112d11aa023f7e4c2c786dce01439439b
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2023-03-27 00:48:32 -0700

    Update with new hash values.

--- test_speed.c
+++ test_speed.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <float.h>
+#include <math.h>
 #include "khashv.h"
 
 #if defined(__MINGW32__) || defined(_WIN32)
@@ -85,9 +86,9 @@ int gig_tests(khashvSeed seed) {
     double   fastest    = DBL_MAX;
 
     const uint32_t hashes[12] = {
-        0xa9ca46b1, 0x8c9f5264, 0x2094ffd9, 0x93946e70, 0x9b71dd71,
-        0x2abeec74, 0x6bca7368, 0x151fff30, 0xc4228495, 0xfad35669,
-        0x9f151590, 0x20a4045b
+        0x8b4c1a33, 0x485105dc, 0xaf1deb0e, 0x2d4a890c, 0x8349b700,
+        0x29a3b3b9, 0xf1ed93ef, 0x8559b73f, 0x11452eff, 0xefa5fe1f,
+        0x5834c363, 0xeb7224a5
     };
 
     printf("Tests on %.3lf GB block: ", gigs);
@@ -112,11 +113,17 @@ int gig_tests(khashvSeed seed) {
     }
 
     double avg = sum / 12;
+    double gbs = get_gbs (avg, gigs);
+    double sec = get_secs(avg);
+    double fgbs = get_gbs(fastest, gigs);
+    gbs  = round(gbs  * 10000.0) / 10000.0;
+    sec  = round(sec  * 10000.0) / 10000.0;
+    fgbs = round(fgbs * 10000.0) / 10000.0;
     printf(
-        "Avg: %lf GB/s, Avg Time: %lf s, Fastest: %lf GB/s\n",
-        get_gbs (avg, gigs),
-        get_secs(avg),
-        get_gbs (fastest, gigs)
+        "Avg: %.4lf GB/s, Avg Time: %.4lf s, Fastest: %.4lf GB/s\n",
+        gbs,
+        sec,
+        fgbs
     );
     fflush(stdout);
     free(bytes);
@@ -137,26 +144,26 @@ int half_mb_tests(khashvSeed seed) {
     double   fastest    = DBL_MAX;
     unsigned count      = 96;
     const uint32_t hashes[96] = {
-        0x249b844b, 0x852e481c, 0xf7ce4779, 0x5b1e79c0, 0xc6280b69,
-        0x18aaed1f, 0x360a7b70, 0x6691373f, 0x62b0e7d2, 0x503f2a13,
-        0x55784198, 0x0449e145, 0xc1fec259, 0xfdde4bcc, 0x3d040585,
-        0x2d54b62c, 0x70f06c7e, 0xcc7a642f, 0xe784348b, 0xe360bb8a,
-        0xd4653bab, 0x129aac4f, 0xdf09ac90, 0xc770d23f, 0x1865b60c,
-        0x366d8ca9, 0x80b13f6f, 0x7317d810, 0x7816b809, 0x919adedb,
-        0x92713259, 0xb15e9216, 0x4cca4cd2, 0xb0bda9b9, 0xa3eb6a63,
-        0x1801f592, 0x7f6ebdfe, 0xcfd5f33c, 0x000c7082, 0x17265e0b,
-        0x6ba10359, 0x8c74f4eb, 0x803f3c08, 0x4ba6860d, 0x0716f9fb,
-        0x6e3c84ae, 0xe77a48f4, 0xc2374c75, 0x97f403ee, 0x3010b84b,
-        0x560ba778, 0x83103235, 0xfd4adabf, 0xa436bcf0, 0xaa8f96dc,
-        0x29922bec, 0xd5468b54, 0x4b1921b8, 0x2a8ce2d5, 0x86e336f4,
-        0x5fab2354, 0x0e07c225, 0xb181782a, 0xe799459f, 0xcf9541fd,
-        0xcd510976, 0xe70010ea, 0x6202cb22, 0x7d253b79, 0x4d047b53,
-        0xbd26b2ba, 0xc1df8a17, 0x48a6ed87, 0xa980b22c, 0x16b27278,
-        0xb5736e7c, 0x368bd0b9, 0xeee76414, 0xfe58e49d, 0xf3500e6d,
-        0xb57df9f5, 0xb52a7ed6, 0xaca79612, 0xccc9f98a, 0xa7140bd0,
-        0x7e45d2f9, 0xb91ddced, 0x9444f706, 0xa477bfb2, 0xcf7e1d5b,
-        0xd95eab3c, 0x737fa6e5, 0x5f548e79, 0x46539426, 0xef41aa94,
-        0xc0357213
+        0x3b181e13, 0x6df3efe4, 0xa1472e2f, 0xe7fe7261, 0x85db611b,
+        0x95b68b46, 0xa4738539, 0xc67cd2b3, 0x4630444d, 0xb357f7a3,
+        0x60ba4613, 0x20d50be8, 0x5908392d, 0xd5c1411e, 0xa315f311,
+        0xe92b8d4a, 0x3504718c, 0x78d5d987, 0xac324986, 0xa9c146a3,
+        0xea4120ac, 0x1ab20115, 0xb4cf0fc0, 0x3726e7c6, 0x781b19b4,
+        0x897a635f, 0x49c879a6, 0x414f698e, 0xef3c3c66, 0x668de11e,
+        0xf6f2af8d, 0x6db89e5f, 0xa2621047, 0x26736838, 0xca8539cf,
+        0xe1e92796, 0xbd178553, 0x31aedc2d, 0x41f4377f, 0x0683f7a2,
+        0xff1d7f6f, 0x4a788c33, 0xb4823086, 0xf3b45106, 0xf2e12a97,
+        0x1505b0e8, 0x32d16f9d, 0xa4ccbd11, 0x61f6aa54, 0x8dc4eb8d,
+        0xe7ac77ca, 0xb00dd338, 0x9330ce85, 0xae721ca9, 0x236eb8a2,
+        0xcd7aba61, 0x2fbd751e, 0x978edc2c, 0x09ef6175, 0x78d12480,
+        0x08b21322, 0x02826493, 0x36244a76, 0xb7e1489c, 0x365c631f,
+        0x08188ea8, 0x92bd6910, 0xa7cf34d0, 0x9b91a005, 0x8c7cfc38,
+        0xf732ae18, 0x87f2f485, 0xa42d236d, 0x967880e3, 0xf04cb79d,
+        0xfd9d613f, 0xfa7ae694, 0xfb680e60, 0x2de7c7c9, 0xa5979af7,
+        0x6b24f6a3, 0xfebb25de, 0x3163a706, 0x7d8d0a35, 0xb5cacfcf,
+        0xdf774e72, 0xd06db96e, 0x16d7e8db, 0xf1e368e7, 0x21efe8d5,
+        0x59d6f29f, 0xb0ee28bc, 0x849b575e, 0x96887453, 0x2eabdd1f,
+        0x3cdc8fa8
     };
 
     printf("Tests on 512 KB block: ");
@@ -181,11 +188,17 @@ int half_mb_tests(khashvSeed seed) {
     }
 
     double avg = sum / count;
+    double gbs = get_gbs  (avg, gigs);
+    double usec = get_usecs(avg);
+    double fgbs = get_gbs  (fastest, gigs);
+    gbs  = round(gbs  * 10000.0) / 10000.0;
+    usec = round(usec * 10000.0) / 10000.0;
+    fgbs = round(fgbs * 10000.0) / 10000.0;
     printf(
-        "Avg: %lf GB/s, Avg Time: %lf us, Fastest: %lf GB/s\n",
-        get_gbs  (avg, gigs),
-        get_usecs(avg),
-        get_gbs  (fastest, gigs)
+        "Avg: %.4lf GB/s, Avg Time: %.4lf us, Fastest: %.4lf GB/s\n",
+        gbs,
+        usec,
+        fgbs
     );
     fflush(stdout);
     free(bytes);
